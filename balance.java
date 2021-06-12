a46b1bf1eb6340c5ba1f515e820cc5763f0fdd0d
profegil <gramirezfl@ipn.mx>
2021-06-12 10:41:31 -0500

    Create balance.java

---
+++ balance.java
@@ -0,0 +1,21 @@
+package empresa;
+
+public class balance {
+	//atributos
+	float infraestructura;
+	float bancos;
+	float insumoA;
+	float insumoB;
+	float enproceso;
+	float mercancias;
+	float clientes;
+	float hipotecas;
+	float proveedores;
+	float capitalsocial;
+	float utilidades;
+	
+	//metodos
+	//constructor
+	public balance() {	}
+
+}
